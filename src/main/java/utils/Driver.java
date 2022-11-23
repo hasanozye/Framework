aab6ca4813775fb2c950bbcf7cefcaa98c298fbd
Mete <hasanozyer06@gmail.com>
2022-11-23 19:44:19 +0300

    driver is created

--- src/main/java/utils/Driver.java
+++ src/main/java/utils/Driver.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Driver {
 
-    private static ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
+    /*private static ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
 
     public static WebDriver getDriver(){
         if (drivers.get() == null) {
@@ -20,6 +20,21 @@ public class Driver {
     public static void quitDriver(){
         if (drivers.get() != null)
             drivers.get().quit();
+    }*/
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        if (driver.get() == null) {
+            WebDriverManager.chromedriver().setup();
+            driver.set(new ChromeDriver());
+        }
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null)
+            driver.get().quit();
     }
 
 
