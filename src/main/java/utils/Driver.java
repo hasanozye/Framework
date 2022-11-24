9ee554288aeadaf15672b903318ff70780dafe9d
Mete <hasanozyer06@gmail.com>
2022-11-24 19:39:02 +0300

    commits has been made

--- src/main/java/utils/Driver.java
+++ src/main/java/utils/Driver.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.safari.SafariDriver;
 
 public class Driver {
 
-    /*private static ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
+    private static ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
 
     public static WebDriver getDriver(){
         return getDriver("chrome");
@@ -46,21 +46,6 @@ public class Driver {
     public static void quitDriver(){
         if (drivers.get() != null)
             drivers.get().quit();
-    }*/
-
-    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-
-    public static WebDriver getDriver() {
-        if (driver.get() == null) {
-            WebDriverManager.chromedriver().setup();
-            driver.set(new ChromeDriver());
-        }
-        return driver.get();
-    }
-
-    public static void quitDriver() {
-        if (driver.get() != null)
-            driver.get().quit();
     }
 
 
