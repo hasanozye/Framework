ac64d939be0956cbc3bbacf6d20137d2de442a76
Mete <hasanozyer06@gmail.com>
2022-11-29 07:35:52 +0300

    29.11.2022

--- src/test/java/denemeler/staticCagri/Main.java
+++ src/test/java/denemeler/staticCagri/Main.java
@@ -2,7 +2,7 @@ package denemeler.staticCagri;
 
 public class Main {
     public static void main(String[] args) {
-        Islem islem = new Islem(0);
+        Islem islem = new Islem(1);
         islem.topla(3);
         islem.cikar(1);
         System.out.println(islem.getResult());
